55eeb7a76930e6f5b683b5f44821c26618289c3b
Keli06001 <keli06001@utb.vaxjo.se>
2024-02-02 12:46:23 +0100

    Add project files.

---
+++ ILU7_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Channels;
+
+namespace ILU7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Max(0));
+
+        }
+        static int Max(int n) 
+        {
+            int u = 0;
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            foreach (int i in list) 
+            { 
+                if(i > u)
+                {
+                    u = i;
+                }
+            }
+            return u;
+        }
+    }
+}
